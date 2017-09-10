c0370956eec077c1f976134cbdb438e854a57e99
hta218 <huynhtuananh21895@gmail.com>
2017-09-10 14:59:51 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenence.hbs');
+});
+
+app.use(express.static(__dirname + '/public')); //anyone can read //register middleware
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my site !'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to load data'
+  });
+});
+
+app.listen(6969, () => {
+  console.log('Server is up on port 6969');
+});
